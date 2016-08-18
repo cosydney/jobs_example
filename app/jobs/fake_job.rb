9f61efffcb1b57e956809cb4944288733da5aa51
Sydney Cohen <chnsydney@gmail.com>
2016-08-18 10:27:44 +0100

    sidekiq

--- app/jobs/fake_job.rb
+++ app/jobs/fake_job.rb
@@ -1,9 +1,9 @@
 class FakeJob < ActiveJob::Base
   queue_as :default
 
-  def perform(*args)
+  def perform
       puts "I'm starting the fake job"
-      sleep 3
+      sleep 30
       puts "OK I'm done now"
   end
 end
